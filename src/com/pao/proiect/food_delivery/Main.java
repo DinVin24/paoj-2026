f382eed9da45435da78f3577c80b564bb91bbd6e
DinVin24 <emisoltuzu@gmail.com>
2026-04-26 23:59:23 +0300

    Proiect Etapa I: implementare completa

--- src/com/pao/proiect/food_delivery/Main.java
+++ src/com/pao/proiect/food_delivery/Main.java
@@ -27,7 +27,7 @@ public class Main {
         ServiciuClient serviciuClient = ServiciuClient.getInstance();
         ServiciuComanda serviciuComanda = ServiciuComanda.getInstance();
 
-        // 1. Adauga restaurante
+        System.out.println("1. Adauga restaurante");
         Restaurant r1 = new Restaurant(1, "Bella Italia", "Italiana",
                 new Adresa("Calea Victoriei 10", "Bucuresti", "010011", "Romania"));
         Restaurant r2 = new Restaurant(2, "Sushi Palace", "Japoneza",
@@ -40,7 +40,7 @@ public class Main {
         System.out.println("Restaurante adaugate (sortate alfabetic):");
         serviciuRestaurant.listeazaTot().forEach(r -> System.out.println("  " + r));
 
-        // 2. Inregistreaza clienti
+        System.out.println("2. Inregistreaza clienti");
         Client c1 = new Client(1, "Ana Ionescu", "ana@mail.com", "0721000001",
                 new Adresa("Str. Florilor 1", "Bucuresti", "010001", "Romania"));
         Client c2 = new Client(2, "Mihai Popescu", "mihai@mail.com", "0721000002",
@@ -50,13 +50,13 @@ public class Main {
         System.out.println("Clienti inregistrati:");
         serviciuClient.listeazaTot().forEach(c -> System.out.println("  " + c));
 
-        // 3. Inregistreaza un sofer
+        System.out.println("3. Inregistreaza un sofer");
         Sofer s1 = new Sofer(1, "Ion Vasile", "ion@sofer.com", "0723000001",
                 new Adresa("Str. Motilor 7", "Bucuresti", "020001", "Romania"),
                 "Motocicleta", "B-00-ION");
         System.out.println("Sofer inregistrat: " + s1);
 
-        // 4. Adauga preparate in meniuri
+        System.out.println("4. Adauga preparate in meniuri");
         Preparat pizza = new Preparat(1, "Pizza Margherita", "Rosie si mozzarella clasica", 32.00);
         Preparat paste = new Preparat(2, "Paste Carbonara", "Paste cremoase cu bacon", 28.50);
         Preparat sushi = new Preparat(3, "Rulou Somon", "8 bucati rulou somon", 45.00);
@@ -70,7 +70,7 @@ public class Main {
         System.out.println("Meniu Bella Italia (sortat dupa pret):");
         serviciuRestaurant.getMeniu(r1.getId()).forEach(p -> System.out.println("  " + p));
 
-        // 5. Plaseaza comenzi
+        System.out.println("5. Plaseaza comenzi");
         List<ArticolComanda> articole1 = Arrays.asList(
                 new ArticolComanda(pizza, 1),
                 new ArticolComanda(paste, 2));
@@ -79,17 +79,17 @@ public class Main {
         List<ArticolComanda> articole2 = Arrays.asList(new ArticolComanda(burger, 1));
         Comanda comanda2 = serviciuComanda.plaseazaComanda(c2, r3, articole2);
 
-        // 6. Asigneaza sofer la comanda
+        System.out.println("6. Asigneaza un sofer la comanda");
         serviciuComanda.asigneazaSofer(comanda1.getId(), s1);
         System.out.println("Sofer disponibil dupa asignare: " + s1.isDisponibil());
 
-        // 7. Cauta restaurante dupa tipul de bucatarie
+        System.out.println("7. Cauta restaurante dupa tipul de bucatarie");
         String tipCautat = "Italiana";
         System.out.println("Restaurante cu bucatarie '" + tipCautat + "':");
         serviciuRestaurant.cautaDupaBucatarie(tipCautat)
                 .forEach(r -> System.out.println("  " + r));
 
-        // 8. Comenzile active ale soferului
+        System.out.println("8. Comenzile active ale soferului");
         List<Comanda> comenziActive = serviciuComanda.listeazaDupaSofer(s1);
         if (comenziActive.isEmpty()) {
             System.out.println("  Nicio comanda activa.");
@@ -97,7 +97,7 @@ public class Main {
             comenziActive.forEach(c -> System.out.println("  " + c));
         }
 
-        // 9. Livreaza comanda si afiseaza istoricul clientului
+        System.out.println("9. Livreaza comanda si afiseaza istoricul clientului");
         serviciuComanda.livreaza(comanda1.getId());
         System.out.println("Sofer disponibil dupa livrare: " + s1.isDisponibil());
         System.out.println("\nIstoricul comenzilor pentru clientul '" + c1.getNume() + "':");
@@ -108,7 +108,7 @@ public class Main {
             istoric.forEach(inr -> System.out.println("  " + inr));
         }
 
-        // 10. Anuleaza comanda
+        System.out.println("10. Anuleaza comanda");
         try {
             serviciuComanda.anuleaza(comanda2.getId());
             System.out.println("Status comanda #" + comanda2.getId() + ": " + comanda2.getStatus());
